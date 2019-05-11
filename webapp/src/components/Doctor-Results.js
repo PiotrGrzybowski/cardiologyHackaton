a25be5f655e98c81e17abbc039ed0610a673d783
Jędrzej Maczan <jedrzejmaczan@gmail.com>
2019-05-11 14:38:02 +0200

    Tables with medical results work

--- webapp/src/components/Doctor-Results.js
+++ webapp/src/components/Doctor-Results.js
@@ -1,14 +1,90 @@
 import React from 'react';
-import {Col, Row} from "antd";
+import {Col, Row, Table} from "antd";
+import data from '../assets/data/dane';
+
+// const dataSourceMock = [{
+//     key: '1',
+//     name: 'Mike',
+//     age: 32,
+//     address: '10 Downing Street'
+// }, {
+//     key: '2',
+//     name: 'John',
+//     age: 42,
+//     address: '10 Downing Street'
+// }];
+
+//    console.log(Object.keys(data)[dataIndex].split('(')[0]);
+//     console.log('wartosci ', Object.values(data)[dataIndex+1]);
+//     console.log('roznice ', Object.values(data)[dataIndex+2]);
+//     console.log('odchylenie ', Object.values(data)[dataIndex+3]);
+
+const columns = [{
+    title: 'Wartość',
+    dataIndex: 'wartosc',
+    key: 'wartosc',
+}, {
+    title: 'Różnica',
+    dataIndex: 'roznica',
+    key: 'roznica',
+}, {
+    title: 'Odchylenie',
+    dataIndex: 'odchylenie',
+    key: 'odchylenie',
+}];
 
 export const DoctorResults = () => {
-    return (
-        <>
+    let dataIndex = 0;
+    const dataLength = Object.keys(data).length;
+
+    const parseToDataSource = (data, dataIndex) => {
+        let dataSource = [];
+
+        for (let i=0; i<6; i++) {
+            const dataSourceElement = {
+                key: i,
+                wartosc: Object.values(Object.values(data)[dataIndex+1])[i],
+                roznica: Object.values(Object.values(data)[dataIndex+2])[i],
+                odchylenie: Object.values(Object.values(data)[dataIndex+3])[i]
+            }
+
+            dataSource.push(dataSourceElement)
+        }
+
+        return dataSource;
+    }
+
+    const table = (dataIndex) => {
+        const dataSource = parseToDataSource(data, dataIndex);
+
+        return (
             <Row type="flex" justify="center">
                 <Col span={12}>
-
+                    <Table
+                        title={() => Object.keys(data)[dataIndex+1].split('(')[0]}
+                        dataSource={dataSource}
+                        columns={columns}
+                        pagination={false}
+                    />
                 </Col>
             </Row>
+        )
+    }
+
+    const tables = []
+
+    while(dataIndex < dataLength -3) {
+        tables.push(table(dataIndex))
+        dataIndex = dataIndex + 3;
+    }
+
+    console.log(tables)
+
+    return (
+        <>
+            {
+                tables
+            }
         </>
     );
 }
